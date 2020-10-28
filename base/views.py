f0ce0bddfa65e75b0e54eaefc81bc8caa1b4d868
nanthu0123 <nanthu0123@gmail.com>
2020-10-28 20:22:11 +0530

    form field data inserted into model field

--- base/views.py
+++ base/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import UserRegister
+
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+
+def create_user(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        print(name, email, password)
+
+        UserRegister.objects.create(
+            Name=name,
+            Email=email,
+            Password=password
+        )
+
+    return redirect('index')
