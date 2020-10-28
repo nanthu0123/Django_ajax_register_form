b63d508a34a49cb5452982040246d58a95f0743d
nanthu0123 <nanthu0123@gmail.com>
2020-10-28 20:19:35 +0530

    model file

--- base/models.py
+++ base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class UserRegister(models.Model):
+    Name = models.CharField(max_length=20)
+    Email = models.EmailField()
+    Password = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.Name
