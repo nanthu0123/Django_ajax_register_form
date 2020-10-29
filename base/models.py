e4c7ee0294fbb3537a51f6bce14f9c05c6870c26
nanthu0123 <nanthu0123@gmail.com>
2020-10-29 19:13:15 +0530

    date field included

--- base/models.py
+++ base/models.py
@@ -5,6 +5,7 @@ class UserRegister(models.Model):
     Name = models.CharField(max_length=20)
     Email = models.EmailField()
     Password = models.CharField(max_length=20)
+    Date = models.CharField(max_length=30,default=True)
 
     def __str__(self) -> str:
         return self.Name
