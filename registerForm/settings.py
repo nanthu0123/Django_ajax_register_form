1432b962fa495725b84bf7816104641a288f850f
nanthu0123 <nanthu0123@gmail.com>
2020-11-06 17:49:20 +0530

    db connected with mysql

--- registerForm/settings.py
+++ registerForm/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'registerForm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoProject',
+        'USER': 'root',
+        'PASSWORD': 'nanthu1234',
+        'HOST': 'localhost',
+        'PORT': '3025',
     }
 }
 
