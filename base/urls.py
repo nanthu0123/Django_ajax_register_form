bae0308098890ad7f7a66b6c805a5d874f75ff09
nanthu0123 <nanthu0123@gmail.com>
2020-11-06 17:46:33 +0530

    url for view page

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.create_user, name='register'),
+    path('view/',views.view_user,name='view_user')
 ]
