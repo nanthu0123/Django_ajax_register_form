e05013c362a7521d13336dd1a505895a839c9bd7
nanthu0123 <nanthu0123@gmail.com>
2020-11-06 17:48:01 +0530

    user view page code

--- base/views.py
+++ base/views.py
@@ -1,4 +1,7 @@
+from django.http.response import JsonResponse
 from django.shortcuts import redirect, render
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib import messages
 from .models import UserRegister
 
 
@@ -11,7 +14,7 @@ def create_user(request):
         name = request.POST['username']
         email = request.POST['email']
         password = request.POST['password1']
-        date=request.POST['datefilter']
+        date = request.POST['datefilter']
 
         print(name, email, password)
 
@@ -21,5 +24,27 @@ def create_user(request):
             Password=password,
             Date=date
         )
-
     return redirect('index')
+
+
+def view_user(request):
+
+    if request.method == 'POST':
+        try:
+            name = request.POST['search']
+            users = UserRegister.objects.get(Name=name)
+            return render(request, 'profile.html', {'user': users})
+
+        except ObjectDoesNotExist:
+            messages.error(request, 'user data does not exist')
+
+    if request.method == 'GET':
+        if 'term' in request.GET:
+            qs = UserRegister.objects.filter(
+                Name__istartswith=request.GET.get('term'))
+            names = []
+            for User in qs:
+                names.append(User.Name)
+            return JsonResponse(names, safe=False)
+
+    return render(request, 'profile.html')
